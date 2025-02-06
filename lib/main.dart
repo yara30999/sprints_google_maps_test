d34da0e09b6eee5598b4418a1663e8ca505386cb
yara30999 <yaranaserelniny@gmail.com>
2025-02-06 13:58:21 +0200

    create google map screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +14,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+  final Completer<GoogleMapController> _controller =
+      Completer<GoogleMapController>();
+
+  final LatLng cairoLocation = const LatLng(30.0444, 31.2357);
+
+  void _onMapCreated(GoogleMapController controller) {
+    _controller.complete(controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Google Map"),
+      ),
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: CameraPosition(target: cairoLocation, zoom: 15),
+        markers: {
+          Marker(
+            markerId: const MarkerId("cairo_marker"),
+            position: cairoLocation,
+            infoWindow: const InfoWindow(
+              title: "Cairo, Egypt",
+              snippet: "Capital of Egypt",
+            ),
+            icon:
+                BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          ),
+        },
+      ),
     );
   }
 }
